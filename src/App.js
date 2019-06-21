69a9ff3f0bee430319fd4679e5bd0186bec80936
JustinPMitchell <j.mitchell.real@gmail.com>
2019-06-20 23:58:18 -0700

    added functions to handle the api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Search from './search/search.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Search />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      authors: [],
+      title: [],
+      publisher: [],
+      picture: [],
+      link: [],
+      genre: [],
+      information: [] 
+    }
+  }
+
+  componentWillMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Search />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/search/search.js
+++ src/search/search.js
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Search() {
-  return (
-    <div className="Search">
-      <form>
-        <input type="text" name="search"></input>
-        <input type="submit" value="search"></input>
-      </form>
-    </div>
-  );
+class Search  extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      query: '',
+      authors: [],
+      title: [],
+      publisher: [],
+      picture: [],
+      link: [],
+      genre: [],
+      information: []
+    }
+
+    this.searchBooks = this.searchBooks.bind(this);
+    this.handleQueryChange = this.handleQueryChange.bind(this);
+  }
+
+  searchBooks() {
+    const googleBooksUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
+    let startI = 1;
+    let maxStartIndex = 21;
+
+    function searchTenBooks(startIndex) {
+	    fetch("https://www.googleapis.com/books/v1/volumes?q=${this.state.query}&startIndex=" + startIndex)
+	      .then(res => res.json())
+	      .then(
+	        (result) => {
+	          if(result.items && startI <= maxStartIndex) {
+	          	for(let i = 0; i < result.items.length; i++) {
+		          console.log(result.items[i].volumeInfo.title);
+		        }
+	          	startI += 10;
+	          	searchTenBooks(startI);
+	          }
+	        });
+	}
+
+	searchTenBooks(startI);
+  }
+
+  handleQueryChange(event) {
+  	this.setState({ query: event.target.value })
+  }
+
+  render() {
+    return (
+	    <div className="Search">
+		    <input type="text" name="search" value={this.state.value} onChange={this.handleQueryChange}></input>
+		    <button onClick={() => this.searchBooks()}>Search</button> 
+	    </div>
+    );
+  }
 }
 
 export default Search;
