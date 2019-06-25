28cb693fff7bade59ac2833c1f09d60577f49e83
JustinPMitchell <j.mitchell.real@gmail.com>
2019-06-24 23:33:06 -0700

    updated naming conventions and added feature to react whenever a user inputs

--- src/search/search.js
+++ src/search/search.js
@@ -5,14 +5,7 @@ class Search  extends Component {
     super(props)
     this.state = {
       query: '',
-      queryWithPlus: '',
-      authors: [],
-      title: [],
-      publisher: [],
-      picture: [],
-      link: [],
-      genre: [],
-      information: []
+      queryWithPlus: ''
     }
 
     this.searchBooks = this.searchBooks.bind(this);
@@ -20,126 +13,134 @@ class Search  extends Component {
   }
 
   searchBooks(e) {
-  	e.preventDefault();
-  	console.log("searching books...");
+  	// prevents the form from refreshing page
+  	if(e)
+	  	e.preventDefault();
+
   	let that = this;
     const googleBooksUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
-    let startI = 1;
-    let maxStartIndex = 21;
-    let authors = [];
-    let title = [];
-    let publisher = [];
-    let picture = [];
-    let link = [];
-    let genre = [];
-    let information = [];
-    let lastChild = document.getElementsByClassName("list")[0].lastElementChild;
+    let startIndex = 1;
+    const maxStartIndex = 21;
 
+    const list = document.getElementsByClassName("list")[0];
+    let lastChild = list.lastElementChild;
+
+    // additional information that might be used
+    // let genres = [];
+    // let informations = [];
+	// genre: json.items[i].volumeInfo.categories
+	// info: json.items[i].volumeInfo.searchInfo.textSnippet
+
+	// removes all books
     while(lastChild) {
-    	document.getElementsByClassName("list")[0].removeChild(lastChild);
-    	lastChild = document.getElementsByClassName("list")[0].lastElementChild;
+    	list.removeChild(lastChild);
+    	lastChild = list.lastElementChild;
     }
 
     function searchTenBooks(startIndex) {
-    	// let url = `https://www.googleapis.com/books/v1/volumes?q=${this.state.query}&startIndex=`;
-    	// let stateQuery = ({this.state.query});
-    	// console.log("THIS IS THE QUERY: " + url);
+    	// every 20 books, a break is created that links to google books
     	if ((startIndex - 1) % 20 === 0 && startIndex !== 1) {
-    		// console.log('THIS HAPPENED', startIndex);
-    		let apiLinkBlock = document.createElement("a");
-    		let apiLinkNode = document.createTextNode("Find more books here");
-    		apiLinkBlock.appendChild(apiLinkNode);
-			apiLinkBlock.href = 'https://books.google.com';
-    		apiLinkBlock.className = "api-link-block";
-    		document.getElementsByClassName("list")[0].appendChild(apiLinkBlock);
+    		let googleLink = document.createElement("a");
+    		let googleLinkNode = document.createTextNode("Find more books here");
+    		googleLink.appendChild(googleLinkNode);
+			googleLink.href = 'https://books.google.com';
+    		googleLink.className = "api-link-block";
+    		list.appendChild(googleLink);
     	}
+    	// accesses the google books api
 	    fetch(`https://www.googleapis.com/books/v1/volumes?q=${that.state.query}&startIndex=` + startIndex)
 	      .then(res => res.json())
-	      .then(
-	        (result) => {
-	          if(result.items && startI <= maxStartIndex) {
+	      .then((result) => {
+	      	  // as long as there are items in the api and the max has not been reached, create an element for each book
+	          if(result.items && startIndex <= maxStartIndex) {
 	          	for(let i = 0; i < result.items.length; i++) {
-					// genre: json.items[i].volumeInfo.categories // this can be an array
-					// info: json.items[i].volumeInfo.searchInfo.textSnippet
 
-				  // create container
-				  let containerElement = document.createElement("div");
-				  containerElement.className = "book-container";
+	          	  let information = result.items[i].volumeInfo;
+
+				  // create book
+				  let book = document.createElement("div");
+				  book.className = "book";
 
 				  // create title
-		          let titleElement = document.createElement("div");
-		          let titleNode = document.createTextNode("No title available");
-		          if(result.items[i].volumeInfo.title) {
-		          	titleNode = document.createTextNode(result.items[i].volumeInfo.title);
+		          let bookTitle = document.createElement("div");
+		          let bookTitleNode = document.createTextNode("No title available");
+		          if(information.title) {
+		          	bookTitleNode = document.createTextNode(information.title);
 		          }
-		          titleElement.appendChild(titleNode);
-		          titleElement.className = "title-element";
-		          containerElement.appendChild(titleElement);	
+		          bookTitle.appendChild(bookTitleNode);
+		          bookTitle.className = "book-title";
+		          book.appendChild(bookTitle);	
 
 				  // create thumbnail and add link
-				  let thumbnailContainer = document.createElement("a");
-				  thumbnailContainer.href = '';
-				  if(result.items[i].volumeInfo.infoLink) {
-				  	thumbnailContainer.href = result.items[i].volumeInfo.infoLink;
+				  let bookThumbnailContainer = document.createElement("a");
+				  bookThumbnailContainer.href = '';
+				  if(information.infoLink) {
+				  	bookThumbnailContainer.href = information.infoLink;
 				  }
-				  thumbnailContainer.className = "thumbnail-container";
-		          let thumbnailElement = document.createElement("img");
-		          thumbnailElement.src = 'https://cdn.pixabay.com/photo/2018/06/26/01/20/connection-lost-3498366_1280.png';
-		          if(result.items[i].volumeInfo.imageLinks) {
-		          	thumbnailElement.src = result.items[i].volumeInfo.imageLinks.thumbnail;
+				  bookThumbnailContainer.className = "book-thumbnail-container";
+		          let bookThumbnail = document.createElement("img");
+		          bookThumbnail.src = 'https://cdn.pixabay.com/photo/2018/06/26/01/20/connection-lost-3498366_1280.png';
+		          if(information.imageLinks) {
+		          	bookThumbnail.src = information.imageLinks.thumbnail;
 		          } else {
-		          	thumbnailElement.style.height = '190px';
+		          	bookThumbnail.style.height = '190px';
 		          }
-				  thumbnailContainer.appendChild(thumbnailElement);		          
-		          containerElement.appendChild(thumbnailContainer);
+				  bookThumbnailContainer.appendChild(bookThumbnail);		          
+		          book.appendChild(bookThumbnailContainer);
 
-				  // set hover
-				  let hoverContainer = document.createElement("div");
-				  hoverContainer.className = "hover-container";
+				  // create a container for author and publisher
+				  let bookOtherInformationContainer = document.createElement("div");
+				  bookOtherInformationContainer.className = "book-other-information-container";
 				  // create author
-		          let authorElement = document.createElement("div");
-		          let authorNode = document.createTextNode("author unknown");
-		          if(result.items[i].volumeInfo.authors) {
-		          	authorNode = document.createTextNode("by " + result.items[i].volumeInfo.authors);
+		          let bookAuthor = document.createElement("div");
+		          let bookAuthorNode = document.createTextNode("author unknown");
+		          if(information.authors) {
+		          	bookAuthorNode = document.createTextNode("by " + information.authors);
 		          }
-		          authorElement.appendChild(authorNode);
-		          authorElement.className = "author-element";
-		          hoverContainer.appendChild(authorElement);		          	          
+		          bookAuthor.appendChild(bookAuthorNode);
+		          bookAuthor.className = "book-author";
+		          bookOtherInformationContainer.appendChild(bookAuthor);		          	          
 		          // create publisher
-		          let publisherElement = document.createElement("div");
-		          let publisherNode = document.createTextNode("\n Publisher: unknown");
-		          if(result.items[i].volumeInfo.publisher) {
-		          	publisherNode = document.createTextNode("\n Publisher: " + result.items[i].volumeInfo.publisher);
+		          let bookPublisher = document.createElement("div");
+		          let bookPublisherNode = document.createTextNode("\n Publisher: unknown");
+		          if(information.publisher) {
+		          	bookPublisherNode = document.createTextNode("\n Publisher: " + information.publisher);
 		          }
-		          publisherElement.appendChild(publisherNode);
-		          publisherElement.className = "publisher-element";
-		          hoverContainer.appendChild(publisherElement);
-		          containerElement.appendChild(hoverContainer);
-
-
-		    // <button onClick={() => this.searchBooks()}>Search</button>
-		          // containerElement.appendChild(linkElement);	          		          		          		          
-		          document.getElementsByClassName("list")[0].appendChild(containerElement);
+		          bookPublisher.appendChild(bookPublisherNode);
+		          bookPublisher.className = "book-publisher";
+		          bookOtherInformationContainer.appendChild(bookPublisher);
+		          book.appendChild(bookOtherInformationContainer);
+         		          		          		          
+		          list.appendChild(book);
 		        }
-	          	startI += 10;
-	          	searchTenBooks(startI);
+
+		        // recursively calls the api for 10 books
+	          	startIndex += 10;
+	          	searchTenBooks(startIndex);
 	          }
 	        });
-
-
-
 	}
-	searchTenBooks(startI);
+
+	// initiates the api call
+	searchTenBooks(startIndex);
   }
 
+  // as a user inputs their query, states are updated
   handleQueryChange(event) {
+  	let searchBox = document.getElementsByClassName("search-box")[0];
+  	// replaces spaces with '+' for the api call
   	let queryWithPlus = event.target.value.replace(/ /g, '+');
-  	this.setState({ query: event.target.value, queryWithPlus: queryWithPlus });
+  	this.setState({ query: event.target.value,
+  					queryWithPlus: queryWithPlus },
+  					(event) => this.searchBooks(event));
+  					// function() { console.log("setState completed", this.state)});
   	if (!event.target.value) {
-		document.getElementsByClassName("search-box")[0].style.background = "";
+  		// shows the watermark
+		searchBox.style.background = "";
   	} else {
-  		document.getElementsByClassName("search-box")[0].style.background = "white";
-  	}
+  		// removes the watermark
+  		searchBox.style.background = "white";
+  	}  	
   }
 
   render() {
