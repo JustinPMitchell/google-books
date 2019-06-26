bfb2593bbe9c3934f120d2d01e0d09642bb866ab
JustinPMitchell <j.mitchell.real@gmail.com>
2019-06-25 19:51:26 -0700

    refactored functions so they are not bundled together and binded to constructor

--- src/App.js
+++ src/App.js
@@ -4,22 +4,11 @@ import Search from './search/search.js';
 require('dotenv').config();
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-    }
-  }
-
-  componentWillMount() {
-
-  }
-
   render() {
     return (
       <div className="App">
         <Search />
-        <div className="list">
-        </div>
+        <div className="list"></div>
         <footer className="footer">Created by <a className="footer-link" target="_blank" href="https://justinpmitchell.github.io/jpm/">Justin Mitchell</a> © 2019</footer>
       </div>
     );
--- src/search/search.js
+++ src/search/search.js
@@ -1,138 +1,168 @@
 import React, {Component} from 'react';
-require('dotenv').config();
 
 class Search  extends Component {
 
   constructor(props) {
-    super(props)
+  	super(props)
     this.state = {
       query: '',
       queryWithPlus: ''
-    }
+  	}
 
     this.searchBooks = this.searchBooks.bind(this);
     this.handleQueryChange = this.handleQueryChange.bind(this);
+    this.createTitle = this.createTitle.bind(this);
+    this.createThumbnail = this.createThumbnail.bind(this);
+    this.createAuthor = this.createAuthor.bind(this);
+    this.createPublisher = this.createPublisher.bind(this);
+    this.createOtherInformation = this.createOtherInformation.bind(this);
+    this.createBook = this.createBook.bind(this);
+    this.searchFortyBooks = this.searchFortyBooks.bind(this);
     this.timeout = null;
+    this.startIndex = 1;
+    this.maxStartIndex = 41;
+    this.list = document.getElementsByClassName("list")[0];
   }
 
-  searchBooks(e) {
+  // creates a new element for the title and appends to the book
+  createTitle(book, title) {
+	let bookTitle = document.createElement("div");
+	let bookTitleNode = document.createTextNode("No title available");
+	if(title) {
+      bookTitleNode = document.createTextNode(title);
+	}
+	bookTitle.appendChild(bookTitleNode);
+	bookTitle.className = "book-title";
+	book.appendChild(bookTitle);
+  }
+
+  // creates a new element for the thumbnail and a link to more information and appends to the book
+  createThumbnail(book, information) {
+    let bookThumbnailContainer = document.createElement("a");
+	bookThumbnailContainer.href = '';
+	if(information.infoLink) {
+	  bookThumbnailContainer.href = information.infoLink;
+	  bookThumbnailContainer.target = '_blank';
+	}
+	bookThumbnailContainer.className = "book-thumbnail-container";
+    let bookThumbnail = document.createElement("img");
+    bookThumbnail.src = 'https://cdn.pixabay.com/photo/2018/06/26/01/20/connection-lost-3498366_1280.png';
+    if(information.imageLinks) {
+      bookThumbnail.src = information.imageLinks.thumbnail;
+    } else {
+      bookThumbnail.style.height = '190px';
+    }
+	bookThumbnailContainer.appendChild(bookThumbnail);		          
+    book.appendChild(bookThumbnailContainer);
+  }
+
+  // creates a new element for the author and appends to the OtherInformation
+  createAuthor(bookOtherInformationContainer, information) {
+    let bookAuthor = document.createElement("div");
+    let bookAuthorNode = document.createTextNode("author unknown");
+    if(information.authors) {
+      bookAuthorNode = document.createTextNode("by " + information.authors);
+    }
+    bookAuthor.appendChild(bookAuthorNode);
+    bookAuthor.className = "book-author";
+    bookOtherInformationContainer.appendChild(bookAuthor);
+  }
+  
+  // creates a new element for the publisher and appends to the OtherInformation
+  createPublisher(bookOtherInformationContainer, information) {
+    let bookPublisher = document.createElement("div");
+    let bookPublisherNode = document.createTextNode("\n Publisher: unknown");
+    if(information.publisher) {
+      bookPublisherNode = document.createTextNode("\n Publisher: " + information.publisher);
+    }
+    bookPublisher.appendChild(bookPublisherNode);
+    bookPublisher.className = "book-publisher";
+    bookOtherInformationContainer.appendChild(bookPublisher);
+  }
+
+  // creates a new element for other information and appends to the book
+  createOtherInformation(book, information) {
+	let that = this;
+	let bookOtherInformationContainer = document.createElement("div");
+	bookOtherInformationContainer.className = "book-other-information-container";
+	that.createAuthor(bookOtherInformationContainer, information);	          	          
+    that.createPublisher(bookOtherInformationContainer, information);
+    book.appendChild(bookOtherInformationContainer);
+  }
+
+  // creates a book and appends it to the list
+  createBook(result, list) {
+    let that = this;
+    if(result.items) {
+      for(let i = 0; i < result.items.length; i++) {
+      	let information = result.items[i].volumeInfo;
+
+		let book = document.createElement("div");
+		book.className = "book";
+
+		that.createTitle(book, information.title);
+		that.createThumbnail(book, information);
+		that.createOtherInformation(book, information);
+
+	    // TODO: utilize other information
+	    // let genres = [];
+	    // let informations = [];
+		// genre: json.items[i].volumeInfo.categories
+		// info: json.items[i].volumeInfo.searchInfo.textSnippet
+ 		          		          		          
+        list.appendChild(book);
+      }
+
+      // recursively calls the api for 40 books
+      that.startIndex += 40;
+      that.searchFortyBooks(that.startIndex, list);
+    }	
+  }
+
+  searchFortyBooks(startIndex, list) {
+    console.log("I've reached the beginning of searchfortybooks ", list)
+    let that = this;
+    if (that.startIndex < that.maxStartIndex) {
+    // every 40 books, a break is created that links to google books
+    if (that.startIndex !== 1) {
+      let googleLink = document.createElement("a");
+      let googleLinkNode = document.createTextNode("Find more books here");
+      googleLink.appendChild(googleLinkNode);
+	  googleLink.href = 'https://books.google.com';
+	  googleLink.target = '_blank';
+      googleLink.className = "api-link-block";
+      list.appendChild(googleLink);
+    }
+
+	// accesses the google books api
+	console.log("fetched");
+	fetch(`https://www.googleapis.com/books/v1/volumes?key=${process.env.REACT_APP_GOOGLE_BOOKS_API}&q=${that.state.queryWithPlus}&maxResults=40&startIndex=` + that.startIndex)
+	  .then(res => res.json())
+	  .then((result) => {
+	  	// as long as there are items in the api and the max has not been reached, create an element for each book
+	  	that.createBook(result, list);
+	  });
+    }
+  }
+
+  searchBooks(event) {
   	// prevents the form from refreshing page
-  	if(e)
-	  	e.preventDefault();
+  	if(event)
+	  event.preventDefault();
 
   	let that = this;
-    const googleBooksUrl = 'https://www.googleapis.com/books/v1?key=AIzaSyDJq43DKyz36PwuZu9EOMLvbocgHn8FTsM/volumes?q=';
-    let startIndex = 1;
-    const maxStartIndex = 161;
-
-    const list = document.getElementsByClassName("list")[0];
+  	that.startIndex = 1;
+  	let list = document.getElementsByClassName("list")[0];
     let lastChild = list.lastElementChild;
 
 	// removes all books
     while(lastChild) {
-    	list.removeChild(lastChild);
-    	lastChild = list.lastElementChild;
+      list.removeChild(lastChild);
+      lastChild = list.lastElementChild;
     }
 
-    function searchTenBooks(startIndex) {
-    	if (startIndex < maxStartIndex) {
-    		// every 40 books, a break is created that links to google books
-    		if (startIndex !== 1) {
-	    		let googleLink = document.createElement("a");
-	    		let googleLinkNode = document.createTextNode("Find more books here");
-	    		googleLink.appendChild(googleLinkNode);
-				googleLink.href = 'https://books.google.com';
-				googleLink.target = '_blank';
-	    		googleLink.className = "api-link-block";
-	    		list.appendChild(googleLink);
-    		}
-
-    		// accesses the google books api
-    		console.log("fetched");
-		    fetch(`https://www.googleapis.com/books/v1/volumes?key=${process.env.REACT_APP_GOOGLE_BOOKS_API}&q=${that.state.queryWithPlus}&maxResults=40&startIndex=` + startIndex)
-		      .then(res => res.json())
-		      .then((result) => {
-		      	  // as long as there are items in the api and the max has not been reached, create an element for each book
-		          if(result.items) {
-		          	for(let i = 0; i < result.items.length; i++) {
-
-		          	  let information = result.items[i].volumeInfo;
-
-					  // create book
-					  let book = document.createElement("div");
-					  book.className = "book";
-
-					  // create title
-			          let bookTitle = document.createElement("div");
-			          let bookTitleNode = document.createTextNode("No title available");
-			          if(information.title) {
-			          	bookTitleNode = document.createTextNode(information.title);
-			          }
-			          bookTitle.appendChild(bookTitleNode);
-			          bookTitle.className = "book-title";
-			          book.appendChild(bookTitle);	
-
-					  // create thumbnail and add link
-					  let bookThumbnailContainer = document.createElement("a");
-					  bookThumbnailContainer.href = '';
-					  if(information.infoLink) {
-					  	bookThumbnailContainer.href = information.infoLink;
-					  	bookThumbnailContainer.target = '_blank';
-					  }
-					  bookThumbnailContainer.className = "book-thumbnail-container";
-			          let bookThumbnail = document.createElement("img");
-			          bookThumbnail.src = 'https://cdn.pixabay.com/photo/2018/06/26/01/20/connection-lost-3498366_1280.png';
-			          if(information.imageLinks) {
-			          	bookThumbnail.src = information.imageLinks.thumbnail;
-			          } else {
-			          	bookThumbnail.style.height = '190px';
-			          }
-					  bookThumbnailContainer.appendChild(bookThumbnail);		          
-			          book.appendChild(bookThumbnailContainer);
-
-					  // create a container for author and publisher
-					  let bookOtherInformationContainer = document.createElement("div");
-					  bookOtherInformationContainer.className = "book-other-information-container";
-					  // create author
-			          let bookAuthor = document.createElement("div");
-			          let bookAuthorNode = document.createTextNode("author unknown");
-			          if(information.authors) {
-			          	bookAuthorNode = document.createTextNode("by " + information.authors);
-			          }
-			          bookAuthor.appendChild(bookAuthorNode);
-			          bookAuthor.className = "book-author";
-			          bookOtherInformationContainer.appendChild(bookAuthor);		          	          
-			          // create publisher
-			          let bookPublisher = document.createElement("div");
-			          let bookPublisherNode = document.createTextNode("\n Publisher: unknown");
-			          if(information.publisher) {
-			          	bookPublisherNode = document.createTextNode("\n Publisher: " + information.publisher);
-			          }
-			          bookPublisher.appendChild(bookPublisherNode);
-			          bookPublisher.className = "book-publisher";
-			          bookOtherInformationContainer.appendChild(bookPublisher);
-			          book.appendChild(bookOtherInformationContainer);
-
-				      // TODO: utilize other information
-				      // let genres = [];
-				      // let informations = [];
-					  // genre: json.items[i].volumeInfo.categories
-					  // info: json.items[i].volumeInfo.searchInfo.textSnippet
-	         		          		          		          
-			          list.appendChild(book);
-			        }
-
-			        // recursively calls the api for 40 books
-		          	startIndex += 40;
-		          	searchTenBooks(startIndex);
-		          }
-		        });
-    	}
-
-	}
-
 	// initiates the api call
-	searchTenBooks(startIndex);
+	that.searchFortyBooks(that.startIndex, list);
   }
 
   // as a user inputs their query, states are updated
@@ -141,34 +171,34 @@ class Search  extends Component {
 	let queryWithPlus = event.target.value.replace(/ /g, '+');
 	let that = this;
 
-	this.setState({ query: event.target.value,
-		queryWithPlus: queryWithPlus });
+	that.setState({ query: event.target.value,
+	  queryWithPlus: queryWithPlus });
 
 	// waits for user to stop typing
-	searchBox.onkeyup = function (e) {
-	    clearTimeout(that.timeout);
+	searchBox.onkeyup = function (event) {
+	  clearTimeout(that.timeout);
 
-	    that.timeout = setTimeout(function () {
-	        that.searchBooks(e);
-	    }, 700);
+	  that.timeout = setTimeout(function () {
+	    that.searchBooks(event);
+	  }, 700);
 	};
 
 	// adds and removes watermark
   	if (!event.target.value) {
-		searchBox.style.background = "";
+	  searchBox.style.background = "";
   	} else {
-  		searchBox.style.background = "white";
+  	  searchBox.style.background = "white";
   	}  	
   }
 
   render() {
     return (
-	    <div className="Search">
-	    	<h1 className="title">Book Search</h1>
-	    	<form onSubmit={(e) => this.searchBooks(e)} className="search-box-form">
-			    <input type="text" name="search" className="search-box" value={this.state.query} onChange={this.handleQueryChange}></input>
-			</form>
-	    </div>
+	  <div className="Search">
+	    <h1 className="title">Book Search</h1>
+	      <form onSubmit={(e) => this.searchBooks(e)} className="search-box-form">
+			<input type="text" name="search" className="search-box" value={this.state.query} onChange={this.handleQueryChange}></input>
+		  </form>
+	  </div>
     );
   }
 }
